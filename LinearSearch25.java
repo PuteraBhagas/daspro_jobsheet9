a7100abde0ff62825f31ff06aabd99f7f2c7e4cc
PuteraBhagas <p.bhagaswara@gmail.com>
2023-11-13 13:46:04 +0700

    Modifikasi percoban ke 4

--- LinearSearch25.java
+++ LinearSearch25.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class LinearSearch25 {
     public static void main(String[] args) {
-        
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
-                hasil = i;
-                break;
-            }
+          int i;
+
+        System.out.print("Masukkan jumlah elemen array : ");
+        int array = sc.nextInt();
+
+        int [] array1 = new int[array];
+        for (i = 0; i < array; i++) {
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            array1[i] = sc.nextInt();  
         }
-
-        System.out.println("Key ada dalam array pada posisi ke-"+hasil);
+        System.out.print("Masukkan key yang ingin dicari : ");
+        int keys = sc.nextInt();
+        for (i = 0; i < array; i++) {
+            if (array1[i] == keys) {
+                System.out.println("Key ada diposisi indeks ke-" + i);
+                break;  
+            } 
+        }
+        if (i == array) {
+            System.out.println("Key tidak ditemukan");    
+        }
     }
 }
